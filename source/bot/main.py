405a7a25af4fccf3404a6067b91c864ff5d7a0d3
Ivan <sociopart@hotmail.com>
2025-03-19 13:03:44 +0400

    feat: locale system and finalize component-based approach

--- source/bot/main.py
+++ source/bot/main.py
@@ -1,37 +1,41 @@
-import asyncio
-import logging
-import sys
-import os
+import asyncio, logging, os, sys
 
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from components.main_menu.component import main_menu_router
-from components.participant_registration.component import pr_router
+import components.main_menu.router as main_menu
+import components.participant_drawer.router as participant_drawer
+import components.participant_registration.router as participant_registration
+import components.program_generator.router as program_generator
+import components.reports_evaluation.router as reports_evaluation
 
 async def main() -> None:
-    """
-    Main function to run the bot.
-    """
+  """
+  Main function to run the bot.
+  """
 
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+  dotenv_dir = join(dirname(__file__), '..', '.env')
+  component_routers = [
+    main_menu.router,
+    participant_drawer.router,
+    participant_registration.router,
+    program_generator.router,
+    reports_evaluation.router
+  ]
 
-    load_dotenv(join(dirname(__file__), '..', '.env'))
+  logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+  load_dotenv(dotenv_dir)
 
-    bot = Bot(token=os.environ.get("TG_BOT_TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher()
+  bot = Bot(token=os.environ.get("TG_BOT_TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+  dp = Dispatcher()
 
-    dp.include_router(main_menu_router)
-    dp.include_router(pr_router)
-    
-    await dp.start_polling(bot)
+  dp.include_routers(*component_routers)
+
+  await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+  asyncio.run(main())
